c9a26fa46da93c84a196167f5bbcc2b2e49ed6f8
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-18 04:47:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
-
-return array.to_sentence
+  return nil if array.nil?
+  return array[0] if array.length == 1
+  return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
 end
